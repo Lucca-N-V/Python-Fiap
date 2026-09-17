efb3ae18859b95c9fa22e35bfae3bb2d2ff7230e
Lucca Neufeld Vecchiatti <lucca.vec@gmail.com>
2026-09-17 08:58:51 -0300

    Update ex051.py

--- Exercicios/ex051.py
+++ Exercicios/ex051.py
@@ -1,6 +1,6 @@
 ma = 0
 me = 0
-fossil = 0
+idoso = 0
 for i in range (7):
     x = int(input('Digite o ano que você nasceu'))
     if 2026 - x >=18 and x > 1926:
@@ -8,9 +8,9 @@ for i in range (7):
     elif 2026 - x <18 and x > 1926:
         me += 1
     else:
-        fossil += 1
-if fossil == 0:
+        idoso += 1
+if idoso == 0:
     print(f'Dentre as 7 pessoas, {ma} já antingiram a maioridade e {me} são ainda menor de idade!')
-elif fossil > 0:
-    print(f'Dentre as 7 pessoas, {ma} já antingiram a maioridade e {me} são ainda menor de idade. Ademais temos {fossil} pessoas que não deveriam estar vivas!')
+elif idoso > 0:
+    print(f'Dentre as 7 pessoas, {ma} já antingiram a maioridade e {me} são ainda menor de idade. Ademais temos {idoso} pessoas que não deveriam estar vivas!')
 
